72269549e498909048ab99e461411dd5494015d7
cmm519 <cmm519@txstate.edu>
2025-10-01 19:34:04 -0500

    Update piece.java

--- src/Piece/piece.java
+++ src/Piece/piece.java
@@ -1,1 +1,7 @@
-//public object deff
+//public object def
+package board;
+
+public abstract class pieces{
+  //need to determine if a move is valid per 
+  //need to track peice status, location in 2d array and weather or not piece is "alive" 
+}
