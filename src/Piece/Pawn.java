307371f73c8352f3bb220d101b4fcf40644a9536
cmm519 <cmm519@txstate.edu>
2025-10-05 16:40:39 -0500

    Update and rename pawn.java to Pawn.java

--- src/Piece/Pawn.java
+++ src/Piece/Pawn.java
@@ -1,7 +1,8 @@
-package board;
-public class pawn extends piece{
+package Board;
+public class Pawn extends piece{
 // define valid moves within 
   //possibly 2 versions of pawn, top moving/ bottom moving
 }
+
 
 
