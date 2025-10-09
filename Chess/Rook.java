c6753ce8f95313907cb95b838fcb2737b6daf53a
sea jay <70167969+seajay-c@users.noreply.github.com>
2025-10-09 16:43:21 -0500

    Verified rook movment, added non move case

--- Chess/Rook.java
+++ Chess/Rook.java
@@ -8,10 +8,20 @@ public class Rook extends Piece{
 	}
 	
 	public boolean validateMove(Piece[][] board, int currentRow, int currentCol, int newRow, int newCol) {
-		// TODO Auto-generated method stub
+		//Rook can move any number of squares along a rank or file, but cannot leap over other pieces.
+		//So first check if it is moving along a rank or file
+		//If it is not, return false
+		//If it is, check if there are any pieces in the way
+		//If there are, return false
+		//If there are not, return true
+		//If it is not moving, return false
+		
 		
 		//System.out.println("currentRow: " + currentRow + " currentCol: " + currentCol + " newRow: " + newRow + " newCol: " + newCol);
-		
+		if(currentRow == newRow && currentCol == newCol){
+			//Did not move
+			return false;
+		}
 		if(currentRow != newRow && currentCol != newCol){
 			//Did not move along one rank/file
 			return false;
@@ -61,7 +71,7 @@ public class Rook extends Piece{
 	}
 	
 	public String toString(){
-		return color.charAt(0) + "R";
+		return color.charAt(0) + "R";//First letter of color + R for Rook
 		
 	}
 
