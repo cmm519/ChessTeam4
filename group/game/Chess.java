9b849f2f9dc9bf90230b3bb95f26c0ec6eaff5fb
enzhang168 <kyr27@txstate.edu>
2025-10-13 05:23:37 -0500

    Update Chess.java with javadoc notes

--- group/game/Chess.java
+++ group/game/Chess.java
@@ -1,7 +1,16 @@
 package game;
 
+/**
+* The main class for the Chess game. This class initializes and starts the game.
+*/
+
 public class Chess {
 
+    /**
+    * Main method to start the Chess game.
+    *
+    * @param args command line arguments (not used).
+    */
     public static void main(String[] args){
         Game game = new Game();
         game.start();
