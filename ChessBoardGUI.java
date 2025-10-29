bb56f17596994901dfee99713c207ea9377058be
En Zhang <kyr27@txstate.edu>
2025-10-29 09:54:02 -0500

    updated with piece move

--- ChessBoardGUI.java
+++ ChessBoardGUI.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ChessBoardGUI{
 	private static final int ROWS = 8;
 	private static final int COLS = 8;
 	private final JPanel[][] gameBoard = new JPanel[ROWS][COLS];
+	private JPanel selectedPanel = null;
+	private JLabel selectedLabel = null;
 
 	
 	//custom lighter colors for the board
@@ -22,7 +26,7 @@ public class ChessBoardGUI{
 		{"\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F"},		
 		{"\u265C","\u265E","\u265D","\u265B","\u265A","\u265D","\u265E","\u265C"}
 	};
- //
+ 
 
 	public ChessBoardGUI() {
 	
@@ -32,10 +36,27 @@ public class ChessBoardGUI{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new GridLayout(ROWS, COLS));
 
-		for (int i=0; i<ROWS; i++){
-			for (int j=0;j<COLS;j++) {
+
+		// create a MouseAdapter instance to handle all clicks
+		MouseAdapter boardListener = new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e){
+				JPanel clickPanel = (JPanel)e.getSource();
+				handleMouseClick(clickPanel);
+			}
+		};
+
+
+
+		for (int i = 0; i < ROWS; i++){
+			for (int j = 0; j < COLS; j++) {
 				JPanel panel = new JPanel(new BorderLayout());
 				panel.setBackground((i+j) % 2 == 0 ? lightColor : darkColor);
+
+				// add listener to each step
+				panel.addMouseListener(boardListener);
+
+
 				String pieces = initBoard[i][j];
 				if(!pieces.isEmpty()){
 					JLabel label = new JLabel(pieces,SwingConstants.CENTER);
@@ -49,6 +70,38 @@ public class ChessBoardGUI{
 		frame.setVisible(true);
 
 	}
+
+	private void handleMouseClick(JPanel clickPanel){
+		if(selectedPanel == null){
+			if(clickPanel.getComponentCount() > 0){
+				selectedPanel = clickPanel;
+				selectedLabel = (JLabel) selectedPanel.getComponent(0);
+				//highlight selection
+				selectedPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN,3)); 
+			}
+		}else{
+				selectedPanel.remove(selectedLabel);
+				selectedPanel.setBorder(null);
+
+				
+        		// Remove any piece in the destination panel
+        		clickPanel.removeAll();
+
+        		// Add the selected piece to the destination
+       	 		clickPanel.add(selectedLabel);
+
+				// Refresh both panels
+				selectedPanel.revalidate();
+				selectedPanel.repaint();
+				clickPanel.revalidate();
+				clickPanel.repaint();
+
+				// Clear selection
+				selectedPanel = null;
+				selectedLabel = null;
+		}
+	}
+
 	public static void main(String[] argc){
 		SwingUtilities.invokeLater(ChessBoardGUI::new); 
     }
